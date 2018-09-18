e93579c112b6b6eb5d8348874f6e30e707ddee61
Michael Smith <michael@Michael-MacBook.local>
2018-09-17 19:39:53 -0600

    Add bundle, add nav buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,6 +9,10 @@ class Header extends Component {
                 <div className="nav-logo">
                     <img className="name-logo" src={NameLogo} alt="Rachio Logo" height="50px" />
                 </div>
+                <div className="nav-buttons">
+                        <p onClick={() => window.location = "/home"}>Device Map</p>
+                        <p onClick={() => window.location = "/"}>Log Out</p>
+                </div>
             </div>
         );
     }
